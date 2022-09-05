cc87d6e37d5c1aa44d49ae6ad20743588fdaad61
FIL708 <filip.zebrowski4013@gmail.com>
2022-09-05 11:41:20 +0200

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import img from "../images/troll-face.png"
+
 export default function Header() {
     return (
         <header>
-            
+            <img 
+                src={img} 
+                alt="troll face"
+                width="40px"
+                className="header--icon" />
+            <h1 className="header--title">MemeGenerator</h1>
+            <h2 className="header--subtitle">React Course - Project 3</h2>
         </header>
     )
 }

b99f803b9d01edb55bfb6c0a35ce0e61445c3473
FIL708 <filip.zebrowski4013@gmail.com>
2022-09-05 12:53:46 +0200

    meme comp styling end

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,18 +1,22 @@
 export default function Meme() {
     return (
         <main className="meme">
-            <form action="">
+            <form action="" className="meme--form">
                 <div className="meme--inputs">
                     <input type="text" className="meme--input meme--topText"/>
                     <input type="text" className="meme--input meme--bottomText"/>
                 </div>
-                <button type="submit">Get a new meme image</button>
+                <button type="submit" className="meme--button">Get a new meme image</button>
             </form>
-            <img 
-                src="" 
-                alt="meme" 
-                loading="lazy"
-                className="meme--image" />
+            <section className="meme--section">
+                <img 
+                    src="https://cdn.pixabay.com/photo/2022/08/09/16/19/sea-7375377_960_720.jpg" 
+                    alt="meme" 
+                    loading="lazy"
+                    className="meme--image" />
+                <p className="meme--text meme--topText">Some text</p>
+                <p className="meme--text meme--bottomText">Some text</p>
+            </section>
         </main>
     )
 }
